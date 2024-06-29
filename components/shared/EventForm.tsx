7e9249480dd9fb4b9bf3560e21b522ef3e0191bc
vanoalexander <ivannorombang11@gmail.com>
2024-06-29 20:26:14 +0800

    update event form

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -204,7 +204,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                     <p className='ml-3 whitespace-nowrap text-gray-600'>Start Date:</p>
                     <DatePicker
                       selected={field.value}
-                      onChange={(date: Date) => field.onChange(date)}
+                      onChange={(date) => field.onChange(date)}
                       showTimeSelect
                       timeInputLabel='Time:'
                       dateFormat="MM/dd/yyyy h:mm aa"
@@ -233,7 +233,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                     <p className='ml-3 whitespace-nowrap text-gray-600'>End Date:</p>
                     <DatePicker
                       selected={field.value}
-                      onChange={(date: Date) => field.onChange(date)}
+                      onChange={(date) => field.onChange(date)}
                       showTimeSelect
                       timeInputLabel='Time:'
                       dateFormat="MM/dd/yyyy h:mm aa"
